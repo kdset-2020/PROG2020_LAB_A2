55baa78f753b900c7f0e97b1add84cde2810254a
kdowner <squirekev@gmail.com>
2020-09-29 12:15:48 -0400

    Update Push test notification of master change for other members of team

--- PROG2020_LAB_A2/PROG2020_LAB_A2.cpp
+++ PROG2020_LAB_A2/PROG2020_LAB_A2.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "This is a branch bugfix test\n";
-
+    std::cout << "This is a branch pull request notification test\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
