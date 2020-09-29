ae12afb4bd9cba7188c546ae1da03a90623e2ff9
devincar <devmcaron@gmail.com>
2020-09-29 13:04:05 -0400

    Added Unit Test 1

--- PROG2020_LAB_A2/PROG2020_LAB_A2.cpp
+++ PROG2020_LAB_A2/PROG2020_LAB_A2.cpp
@@ -1,12 +1,28 @@
 // PROG2020_LAB_A2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "This is a branch bugfix test\n";
-    // test comment - devin
+
+    // unit test 1
+    // ------------
+    double testSideOne = 0;
+    double testSideTwo = 0;
+
+    cout << "Please enter the Triangle's first side: ");
+    cin >> testSideOne;
+    cout << "Please enter the Triangle's second side: ");
+    cin >> testSideTwo;  
+
+    Triangle testTriangle(testSideOne, testSideTwo);
+    testTriangle.Show();
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
